e275c0a9c45dc00ca626c1110edd6712d2c5ff1c
Brayan René Guerra Rodriguez <bguerrar5@miumg.edu.gt>
2025-04-05 12:37:15 -0600

    Add files via upload

---
+++ proyecto final 1/proyecto final 1/Form5.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace proyecto_final_1
+{
+    partial class Form5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtidAlimentacion = new System.Windows.Forms.TextBox();
+            this.txtcantidad_consumo = new System.Windows.Forms.TextBox();
+            this.txtidUsuario = new System.Windows.Forms.TextBox();
+            this.txtidProducto = new System.Windows.Forms.TextBox();
+            this.txtidUnidadMedida = new System.Windows.Forms.TextBox();
+            this.txtFechaAlimentacion = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(269, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ALIMENTACION PACIENTE";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ID ALIMENTACION";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 155);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "ID USUARIO";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(243, 90);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(154, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "FECHA ALIMENTACION";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(467, 90);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "ID PRODUCTO";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(467, 155);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(148, 16);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "CANTIDAD CONSUMO";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(243, 155);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(130, 16);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "ID UNIDAD MEDIDA";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(215, 229);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "GUARDAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(341, 229);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "SIGUIENTE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtidAlimentacion
+            // 
+            this.txtidAlimentacion.Location = new System.Drawing.Point(43, 120);
+            this.txtidAlimentacion.Name = "txtidAlimentacion";
+            this.txtidAlimentacion.Size = new System.Drawing.Size(100, 22);
+            this.txtidAlimentacion.TabIndex = 9;
+            // 
+            // txtcantidad_consumo
+            // 
+            this.txtcantidad_consumo.Location = new System.Drawing.Point(470, 174);
+            this.txtcantidad_consumo.Name = "txtcantidad_consumo";
+            this.txtcantidad_consumo.Size = new System.Drawing.Size(100, 22);
+            this.txtcantidad_consumo.TabIndex = 10;
+            // 
+            // txtidUsuario
+            // 
+            this.txtidUsuario.Location = new System.Drawing.Point(43, 174);
+            this.txtidUsuario.Name = "txtidUsuario";
+            this.txtidUsuario.Size = new System.Drawing.Size(100, 22);
+            this.txtidUsuario.TabIndex = 11;
+            // 
+            // txtidProducto
+            // 
+            this.txtidProducto.Location = new System.Drawing.Point(470, 120);
+            this.txtidProducto.Name = "txtidProducto";
+            this.txtidProducto.Size = new System.Drawing.Size(100, 22);
+            this.txtidProducto.TabIndex = 12;
+            // 
+            // txtidUnidadMedida
+            // 
+            this.txtidUnidadMedida.Location = new System.Drawing.Point(246, 174);
+            this.txtidUnidadMedida.Name = "txtidUnidadMedida";
+            this.txtidUnidadMedida.Size = new System.Drawing.Size(100, 22);
+            this.txtidUnidadMedida.TabIndex = 13;
+            // 
+            // txtFechaAlimentacion
+            // 
+            this.txtFechaAlimentacion.Location = new System.Drawing.Point(246, 120);
+            this.txtFechaAlimentacion.Name = "txtFechaAlimentacion";
+            this.txtFechaAlimentacion.Size = new System.Drawing.Size(100, 22);
+            this.txtFechaAlimentacion.TabIndex = 14;
+            // 
+            // Form5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(657, 286);
+            this.Controls.Add(this.txtFechaAlimentacion);
+            this.Controls.Add(this.txtidUnidadMedida);
+            this.Controls.Add(this.txtidProducto);
+            this.Controls.Add(this.txtidUsuario);
+            this.Controls.Add(this.txtcantidad_consumo);
+            this.Controls.Add(this.txtidAlimentacion);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form5";
+            this.Text = "Form5";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtidAlimentacion;
+        private System.Windows.Forms.TextBox txtcantidad_consumo;
+        private System.Windows.Forms.TextBox txtidUsuario;
+        private System.Windows.Forms.TextBox txtidProducto;
+        private System.Windows.Forms.TextBox txtidUnidadMedida;
+        private System.Windows.Forms.TextBox txtFechaAlimentacion;
+    }
+}
---
+++ proyecto final 1/proyecto final 1/Form8.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace proyecto_final_1
+{
+    partial class Form8
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxIdUsuario = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // comboBoxIdUsuario
+            // 
+            this.comboBoxIdUsuario.FormattingEnabled = true;
+            this.comboBoxIdUsuario.Location = new System.Drawing.Point(33, 77);
+            this.comboBoxIdUsuario.Name = "comboBoxIdUsuario";
+            this.comboBoxIdUsuario.Size = new System.Drawing.Size(227, 24);
+            this.comboBoxIdUsuario.TabIndex = 0;
+            this.comboBoxIdUsuario.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form8
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBoxIdUsuario);
+            this.Name = "Form8";
+            this.Text = "Form8";
+            this.Load += new System.EventHandler(this.Form8_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxIdUsuario;
+    }
+}
