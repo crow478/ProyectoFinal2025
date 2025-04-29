d9ecce617c35dcca43d6fbb7219856b8c9adeb17
Brayan René Guerra Rodriguez <bguerrar5@miumg.edu.gt>
2025-04-28 22:08:39 -0600

    Add files via upload
    ingeniero no me dejo subir la carpeta por que me tira que son muchos archivos e intente en un rar y me decia que solo 25 mg eran las aceptables y el rar pesaba 26

---
+++ Form5.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace proyecto_final_1
+{
+    partial class Form5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form5));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtidAlimentacion = new System.Windows.Forms.TextBox();
+            this.txtcantidad_consumo = new System.Windows.Forms.TextBox();
+            this.txtidUsuario = new System.Windows.Forms.TextBox();
+            this.txtidProducto = new System.Windows.Forms.TextBox();
+            this.txtidUnidadMedida = new System.Windows.Forms.TextBox();
+            this.txtFechaAlimentacion = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Javanese Text", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(25, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(292, 43);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ALIMENTACION PACIENTE";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Javanese Text", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(60, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(144, 32);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ID ALIMENTACION";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Javanese Text", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(104, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 32);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "ID USUARIO";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Javanese Text", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(35, 222);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(177, 32);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "FECHA ALIMENTACION";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Javanese Text", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label5.Location = new System.Drawing.Point(96, 183);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(111, 32);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "ID PRODUCTO";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Javanese Text", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label6.Location = new System.Drawing.Point(35, 252);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(165, 32);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "CANTIDAD CONSUMO";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Javanese Text", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label7.Location = new System.Drawing.Point(47, 139);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(150, 32);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "ID UNIDAD MEDIDA";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LimeGreen;
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(103, 322);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 25);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "GUARDAR";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LimeGreen;
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(621, 353);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 28);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "SIGUIENTE";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtidAlimentacion
+            // 
+            this.txtidAlimentacion.Location = new System.Drawing.Point(215, 69);
+            this.txtidAlimentacion.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtidAlimentacion.Name = "txtidAlimentacion";
+            this.txtidAlimentacion.Size = new System.Drawing.Size(95, 22);
+            this.txtidAlimentacion.TabIndex = 9;
+            // 
+            // txtcantidad_consumo
+            // 
+            this.txtcantidad_consumo.Location = new System.Drawing.Point(215, 255);
+            this.txtcantidad_consumo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtcantidad_consumo.Name = "txtcantidad_consumo";
+            this.txtcantidad_consumo.Size = new System.Drawing.Size(100, 22);
+            this.txtcantidad_consumo.TabIndex = 10;
+            // 
+            // txtidUsuario
+            // 
+            this.txtidUsuario.Location = new System.Drawing.Point(215, 111);
+            this.txtidUsuario.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtidUsuario.Name = "txtidUsuario";
+            this.txtidUsuario.Size = new System.Drawing.Size(95, 22);
+            this.txtidUsuario.TabIndex = 11;
+            // 
+            // txtidProducto
+            // 
+            this.txtidProducto.Location = new System.Drawing.Point(215, 183);
+            this.txtidProducto.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtidProducto.Name = "txtidProducto";
+            this.txtidProducto.Size = new System.Drawing.Size(95, 22);
+            this.txtidProducto.TabIndex = 12;
+            // 
+            // txtidUnidadMedida
+            // 
+            this.txtidUnidadMedida.Location = new System.Drawing.Point(215, 145);
+            this.txtidUnidadMedida.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtidUnidadMedida.Name = "txtidUnidadMedida";
+            this.txtidUnidadMedida.Size = new System.Drawing.Size(95, 22);
+            this.txtidUnidadMedida.TabIndex = 13;
+            // 
+            // txtFechaAlimentacion
+            // 
+            this.txtFechaAlimentacion.Location = new System.Drawing.Point(215, 222);
+            this.txtFechaAlimentacion.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtFechaAlimentacion.Name = "txtFechaAlimentacion";
+            this.txtFechaAlimentacion.Size = new System.Drawing.Size(100, 22);
+            this.txtFechaAlimentacion.TabIndex = 14;
+            // 
+            // Form5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(733, 375);
+            this.Controls.Add(this.txtFechaAlimentacion);
+            this.Controls.Add(this.txtidUnidadMedida);
+            this.Controls.Add(this.txtidProducto);
+            this.Controls.Add(this.txtidUsuario);
+            this.Controls.Add(this.txtcantidad_consumo);
+            this.Controls.Add(this.txtidAlimentacion);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form5";
+            this.Text = "Form5";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtidAlimentacion;
+        private System.Windows.Forms.TextBox txtcantidad_consumo;
+        private System.Windows.Forms.TextBox txtidUsuario;
+        private System.Windows.Forms.TextBox txtidProducto;
+        private System.Windows.Forms.TextBox txtidUnidadMedida;
+        private System.Windows.Forms.TextBox txtFechaAlimentacion;
+    }
+}
